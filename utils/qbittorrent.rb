d28e8bad33d067749d2b04ed178daabc61b3bf09
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-04-16 12:43:45 +0200

    QBitTorrent: update to API v2.4.1

--- utils/qbittorrent.rb
+++ utils/qbittorrent.rb
@@ -3,6 +3,7 @@ require 'json'
 
 module Utils
 
+# https://github.com/qbittorrent/qBittorrent/wiki/Web-API-Documentation
 class QBitTorrent
   def initialize(uri, log: Log.new)
     @log = log
@@ -12,16 +13,16 @@ class QBitTorrent
   end
 
   def download_limit=(n)
-    @log.debug "setting download limit to: %d bytes" % n
-    post! "/command/setGlobalDlLimit", 'limit' => n
+    @log.info "setting download limit to: %d bytes" % n
+    post! "/api/v2/transfer/setDownloadLimit", 'limit' => n
   end
 
   def pause_downloading
-    downloading.each { |t| pause t unless t.fetch("state") == "pausedDL" }
+    pause downloading.select { |t| t.fetch("state") != "pausedDL" }
   end
 
   def resume_downloading
-    downloading.each { |t| resume t if t.fetch("state") == "pausedDL" }
+    resume downloading.select { |t| t.fetch("state") == "pausedDL" }
   end
 
   %i[all downloading completed paused active inactive].each do |filter|
@@ -32,38 +33,33 @@ class QBitTorrent
 
   def torrents(filter: nil)
     q = {filter: filter}
-    uri = add_uri "/query/torrents", q.reject { |k,v| v.nil? }
+    uri = add_uri "/api/v2/torrents/info", q.reject { |k,v| v.nil? }
 
     JSON.parse(get!(uri).body).tap do |ts|
       @log[q: q].debug "fetched %d torrents" % ts.size
     end
   end
 
-  def torrent_files(t)
-    JSON.parse get!("/query/propertiesFiles/#{t.fetch "hash"}").body
-  end
-
-  def delete_perm(t)
-    tlog(t).debug "deleting torrent (with data)"
-    post! "/command/deletePerm", 'hashes' => t.fetch("hash")
+  def delete_perm(ts)
+    post_hashes! "/api/v2/torrents/delete", ts, 'deleteFiles' => 'true' do |log|
+      log.info "deleting (with data)"
+    end
   end
 
   private def tlog(t)
     @log[torrent: t.fetch("name")]
   end
 
-  private def pause(t)
-    tlog(t).debug "pausing torrent"
-    post_hash! "/command/pause", t
+  private def pause(ts)
+    post_hashes! "/api/v2/torrents/pause", ts do |log|
+      log.info "pausing"
+    end
   end
 
-  private def resume(t)
-    tlog(t).debug "resuming torrent"
-    post_hash! "/command/resume", t
-  end
-
-  private def post_hash!(path, t)
-    post! path, 'hash' => t.fetch("hash")
+  private def resume(ts)
+    post_hashes! "/api/v2/torrents/resume", ts do |log|
+      log.info "resuming"
+    end
   end
 
   private def get!(path)
@@ -76,8 +72,23 @@ class QBitTorrent
     request! req
   end
 
+  private def post_hashes!(path, ts, data={})
+    ts, log =
+      if Array === ts
+        [ts, @log[torrents: ts.size]]
+      else
+        [[ts], tlog(ts)]
+      end
+    if ts.empty?
+      log[path: path].debug "no hashes to post"
+      return
+    end
+    yield log if block_given?
+    post! path, data.merge('hashes' => ts.map { |t| t.fetch "hash" } * "|")
+  end
+
   private def set_cookie!(user, password)
-    req = new_req :Post, "/login"
+    req = new_req :Post, "/api/v2/auth/login"
     req.form_data = {'username' => user, 'password' => password}
 
     @cookie = request!(req)['set-cookie'].
