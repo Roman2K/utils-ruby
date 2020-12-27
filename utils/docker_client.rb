bdb271745606af1f9caa108c3e5177c151656ae2
Roman Le Négrate <roman.lenegrate@gmail.com>
2020-12-27 02:46:21 +0100

    DockerClient: add #container_{exec,restart}

--- utils/docker_client.rb
+++ utils/docker_client.rb
@@ -1,17 +1,47 @@
 require 'net_x/http_unix'
+require 'open3'
 
 module Utils
 
 class DockerClient
   API_VER = "1.40"
 
-  def initialize(uri)
-    uri = NetX::HTTPUnix.new 'unix://' + uri.path if uri.scheme == 'unix'
-    @client = SimpleHTTP.new uri, json: true
+  def initialize(uri, log:)
+    @docker_host = self.class.fix_unix_url(uri) { _1 }.to_s
+    @client = SimpleHTTP.new \
+      self.class.fix_unix_url(uri) { NetX::HTTPUnix.new _1 },
+      json: true,
+      log: log["http"]
+  end
+
+  def self.fix_unix_url(uri)
+    case uri.scheme
+    when 'unix'
+      yield \
+        case uri.to_s
+        when %r{^unix://.+} then $&
+        else "unix://#{uri.path}"
+        end
+    else
+      uri
+    end
   end
 
   def get_json(path)
     @client.get path
+  end
+
+  def container_exec(id, *cmd, &block)
+    Open3.popen3(
+      {"DOCKER_HOST" => @docker_host},
+      "docker", "exec", id, *cmd.map(&:to_s),
+      &block
+    )
+  end
+
+  def container_restart(id)
+    @client.post "/containers/#{id}/restart", nil, expect: [Net::HTTPNoContent],
+      json_out: false
   end
 end
 
